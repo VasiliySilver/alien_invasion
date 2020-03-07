119304d3639495fb1fae266fb94f08edf5b2c72b
VasiliySilver <s555133@mail.ru>
2020-03-08 00:47:33 +0300

    Жопа

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,8 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
 
+
+жопа
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
     def run_game(self):
